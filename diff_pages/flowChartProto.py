93759e5fcb8eacdb52c99b4a6966824fb03be188
Jovin12 <jovinantony12123@gmail.com>
2026-03-30 12:01:01 -0400

    modified flow chart to auto connect

--- diff_pages/flowChartProto.py
+++ diff_pages/flowChartProto.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from streamlit_flow import streamlit_flow
-from streamlit_flow.elements import StreamlitFlowNode
+from streamlit_flow.elements import StreamlitFlowNode, StreamlitFlowEdge
 from streamlit_flow.state import StreamlitFlowState
 
 def flow_page():
-    # Injecting CSS
+    # Injecting CSS for styling
     st.markdown("""
     <style>
 
@@ -68,7 +68,7 @@ def flow_page():
     /* === DARK COLUMN BACKGROUNDS === */
 div[data-testid="stHorizontalBlock"] > div:nth-of-type(1),
 div[data-testid="stHorizontalBlock"] > div:nth-of-type(2) {
-    background-color: #484848;  /* dark gray */
+    background-color: #686868;  /* dark gray */
     border-radius: 12px;
 }
 
@@ -98,8 +98,6 @@ hr {
 
     </style>
     """, unsafe_allow_html=True)
-
-
     NODE_COLORS = {
         "input": "#4a86e8",
         "backend": "#744ae8",
@@ -111,31 +109,11 @@ hr {
         "transpile": "#4F30EA"
     }
 
-
-
-    # Session state (UNCHANGED)
+    # Initialize Session State
     if 'flow_state' not in st.session_state:
         st.session_state.flow_state = StreamlitFlowState([], [])
-    if 'input_node_id' not in st.session_state:
-        st.session_state.input_node_id = 0
-    if 'node_id' not in st.session_state:
-        st.session_state.node_id = 0
     if 'backend_name' not in st.session_state:
-        st.session_state.backend_name = "IbmFez"
-    if 'compress_node_id' not in st.session_state: 
-        st.session_state.compress_node_id = 0
-    if 'qucad_node_id' not in st.session_state: 
-        st.session_state.qucad_node_id = 0
-    if 'no_comp_node_id' not in st.session_state: 
-        st.session_state.no_comp_node_id = 0
-    if 'qbound_node_id' not in st.session_state: 
-        st.session_state.qbound_node_id = 0
-    if 'simple_node_id' not in st.session_state:
-        st.session_state.simple_node_id = 0
-    if 'transpile_node' not in st.session_state:
-        st.session_state.transpile_node = 'Transpile'
-    if 'selected_node' not in st.session_state: 
-        st.session_state.selected_node = None
+        st.session_state.backend_name = "ibm_fez"
 
     st.set_page_config(layout="wide")
 
@@ -145,178 +123,67 @@ hr {
         st.subheader("Node Palette")
         st.divider()
 
-        with st.container(height=600, border=True):
+        # Helper function to prevent duplicates by checking ID
+        def add_unique_node(node_id, content, color, node_type='default', label=None):
+            # Filter out any existing node with this ID
+            st.session_state.flow_state.nodes = [n for n in st.session_state.flow_state.nodes if n.id != node_id]
+            
+            new_node = StreamlitFlowNode(
+                id=node_id, 
+                pos=(50, 50),
+                data={'content': content},
+                node_type=node_type,
+                label=label if label else content,
+                draggable=True,
+                style={"backgroundColor": color, "color": "black" if color in ["#FFC878", "#FFD700"] else "white"},
+                selectable=True,
+                deletable=True,
+            )
+            st.session_state.flow_state.nodes.append(new_node)
 
+        with st.container(height=600, border=True):
             st.subheader("Input Nodes")
             c1, c2 = st.columns(2)
-
             with c1:
                 if st.button("Circuit Node", key="btn_input"):
-                    st.session_state.input_node_id += 1
-                    id = "input_" + str(st.session_state.input_node_id)
-                    new_id = str(st.session_state.input_node_id)
-                    new_node = StreamlitFlowNode(
-                        id=id, pos=(50, 50),
-                        data={
-                            'content': f'Circuit_{new_id}',
-                            'color': NODE_COLORS["input"]
-                        },
-                        node_type='input',
-                        label=f'Input {new_id}',
-                        draggable=True,
-                        style={"backgroundColor": NODE_COLORS["input"]},
-                        selectable=True,
-                        deletable=True,
-                    )
-                    st.session_state.flow_state.nodes.append(new_node)
-                    # st.rerun()
-
+                    add_unique_node("input_node", "Circuit", NODE_COLORS["input"], "input")
             with c2:
-                if st.button("Circuit Backend Node", key="btn_backend"):
-                    new_name = str(st.session_state.backend_name)
-                    new_node = StreamlitFlowNode(
-                        id=new_name, pos=(50, 50),
-                        data={'content': f'Backend'},
-                        node_type='input',
-                        label=f'Input {new_name}',
-                        draggable=True,
-                        style = {'backgroundColor': NODE_COLORS['backend']},
-                        selectable=True,
-                        deletable=True,
-                    )
-                    st.session_state.flow_state.nodes.append(new_node)
-                    # st.rerun()
+                if st.button("Backend Node", key="btn_backend"):
+                    add_unique_node("backend_node", "Backend", NODE_COLORS["backend"], "input")
 
             st.divider()
-
             st.subheader("Compression Nodes")
             c1, c2 = st.columns(2)
-
             with c1:
-                if st.button(" CompressVQC Node", key="btn_compress"):
-                    st.session_state.compress_node_id += 1
-                    id = "compress_" + str(st.session_state.compress_node_id)
-                    new_id = str(st.session_state.compress_node_id)
-                    new_node = StreamlitFlowNode(
-                        id=id, pos=(50, 50),
-                        data={
-                            'content': f'CompressVQC{new_id}',
-                            'color': NODE_COLORS["compress"]
-                        },
-                        node_type='default',
-                        label=f'Process {new_id}',
-                        draggable=True,
-                        style={"backgroundColor": NODE_COLORS["compress"]},
-                        selectable=True,
-                        deletable=True,
-                    )
-                    st.session_state.flow_state.nodes.append(new_node)
-                    # st.rerun()
-
+                if st.button("CompressVQC", key="btn_compress"):
+                    add_unique_node("compress_node", "CompressVQC", NODE_COLORS["compress"])
             with c2:
-                if st.button("QuCAD Node", key="btn_qucad"):
-                    st.session_state.qucad_node_id += 1
-                    id = "qucad_" + str(st.session_state.qucad_node_id)
-                    new_id = str(st.session_state.qucad_node_id)
-                    new_node = StreamlitFlowNode(
-                        id=id, pos=(50, 50),
-                        data={'content': f'QuCAD_{new_id}'},
-                        node_type='default',
-                        label=f'Process {new_id}',
-                        draggable=True,
-                        selectable=True,
-                        deletable=True,
-                        style={"backgroundColor": NODE_COLORS["qucad"]}
-                    )
-                    st.session_state.flow_state.nodes.append(new_node)
-                    # st.rerun()
-
-            c1, c2, c3 = st.columns([1,2,1])
-            with c2:
-                if st.button("NoCompression Node", key="btn_nocomp"):
-                    st.session_state.no_comp_node_id += 1
-                    id = 'nocomp_' + str(st.session_state.no_comp_node_id)
-                    new_id = str(st.session_state.no_comp_node_id)
-                    new_node = StreamlitFlowNode(
-                        id=id, pos=(50, 50),
-                        data={'content': f'NoCompression_{new_id}'},
-                        node_type='default',
-                        label=f'Process {new_id}',
-                        draggable=True,
-                        selectable=True,
-                        deletable=True,
-                        style={"backgroundColor": NODE_COLORS["nocomp"]}
-                    )
-                    st.session_state.flow_state.nodes.append(new_node)
-                    # st.rerun()
+                if st.button("QuCAD", key="btn_qucad"):
+                    add_unique_node("qucad_node", "QuCAD", NODE_COLORS["qucad"])
+            
+            if st.button("NoCompression", key="btn_nocomp"):
+                add_unique_node("nocomp_node", "NoCompression", NODE_COLORS["nocomp"])
 
             st.divider()
-
             st.subheader("Fidelity Nodes")
             c1, c2 = st.columns(2)
-
             with c1:
-                if st.button("QuBound Node", key="btn_qbound"):
-                    st.session_state.qbound_node_id += 1
-                    id = 'qbound_' + str(st.session_state.qbound_node_id)
-                    new_id = str(st.session_state.qbound_node_id)
-                    new_node = StreamlitFlowNode(
-                        id=id, pos=(50, 50),
-                        data={'content': f'QuBound_{new_id}'},
-                        node_type='default',
-                        label=f'Process {new_id}',
-                        draggable=True,
-                        style={"backgroundColor": NODE_COLORS["qbound"]},
-                        selectable=True,
-                        deletable=True,
-                    )
-                    st.session_state.flow_state.nodes.append(new_node)
-                    # st.rerun()
-
+                if st.button("QuBound", key="btn_qbound"):
+                    add_unique_node("qbound_node", "QuBound", NODE_COLORS["qbound"])
             with c2:
-                if st.button("Simple/Simulated Fidelity Node", key="btn_simple"):
-                    st.session_state.simple_node_id += 1
-                    id = 'simple_' + str(st.session_state.simple_node_id)
-                    new_id = str(st.session_state.simple_node_id)
-                    new_node = StreamlitFlowNode(
-                        id=id, pos=(50, 50),
-                        data={'content': f'SimpleFid_{new_id}'},
-                        node_type='default',
-                        label=f'Process {new_id}',
-                        draggable=True,
-                        selectable=True,
-                        deletable=True,
-                        style={"backgroundColor": NODE_COLORS["simple"]}
-                    )
-                    st.session_state.flow_state.nodes.append(new_node)
-                    # st.rerun()
+                if st.button("Simple Fid", key="btn_simple"):
+                    add_unique_node("simple_node", "SimpleFid", NODE_COLORS["simple"])
 
             st.divider()
-
-            st.subheader("Transpilation Nodes")
-
-            c1, c2, c3 = st.columns([1,2,1])
-            with c2:
-                if st.button("Transpilation Node", key="btn_transpile"):
-                    unique_transpile_id = "transpile_unique"
-                    new_node = StreamlitFlowNode(
-                        id=unique_transpile_id, pos=(50, 50),
-                        data={'content': f'Transpile'},
-                        node_type='default',
-                        label=f'Process {unique_transpile_id}',
-                        draggable=True,
-                        selectable=True,
-                        deletable=True,
-                        style={"backgroundColor": NODE_COLORS["transpile"]}
-                    )
-                    st.session_state.flow_state.nodes.append(new_node)
-                    # st.rerun()
-
-                st.divider()
+            st.subheader("Transpilation")
+            if st.button("Transpile Node", key="btn_transpile"):
+                add_unique_node("transpile_node", "Transpile", NODE_COLORS["transpile"])
 
     with col1:
+
+        # Render Flow
         flow_state = streamlit_flow(
-            'my_flow', 
+            'quantum_flow', 
             state=st.session_state.flow_state,
             height=500,
             fit_view=True,
@@ -325,71 +192,86 @@ hr {
             animate_new_edges=True,      
             enable_node_menu=True,       
             enable_edge_menu=True, 
-            get_node_on_click=True,
-            get_edge_on_click=True      
+            get_node_on_click=True
         )
 
         if flow_state is not None:
             st.session_state.flow_state = flow_state
 
-            # st.write(st.session_state.flow_state)
-            # st.write(st.session_state.flow_state.selected_id)
+        # Node Property Editor
+        if st.session_state.flow_state.selected_id:
+            node_id = st.session_state.flow_state.selected_id
+            st.divider()
+            st.subheader(f"Settings: {node_id}")
 
-            with st.container():
-                if st.session_state.flow_state.selected_id is not None:
+            if "input_node" == node_id:
+                st.file_uploader("Upload Circuit (.qpy)", type="qpy")
+            
+            elif "backend_node" == node_id:
+                backend = st.selectbox("Select Target Backend:", ["ibm_fez", "ibm_marrakesh", "ibm_torino"])
+                st.session_state.backend_name = backend
 
-                    st.session_state.selected_node = st.session_state.flow_state.selected_id
-                    node_name = st.session_state.selected_node
-                    st.header(node_name)
+            elif "qucad_node" == node_id:
+                st.info("QuCAD requires noise profile from Backend node.")
+                st.radio("Optimization Mode", ["Start Fresh", "Use Previous QNN"], horizontal=True)
 
-                    if "input" in str(node_name):
-                        st.file_uploader(label="Upload your circuit qpy file Here:", type="qpy")
-                    
-                    if "IbmFez" in str(node_name):
-                        backend = st.selectbox("Select Backends:", options=["ibm_fez", "ibm_marrakesh", "ibm_torino"])
-                        st.session_state.backend_name = backend
+            elif "transpile_node" == node_id:
+                st.slider("Qiskit Optimization Level", 0, 3, 1)
 
-                    if "qucad" in str(node_name):
-                        QuCAD_option = st.radio(
-                            label="noise_opt",
-                            options=["User Previous QNN optimization","Start Fresh"],
-                            index=1,
-                            horizontal=True,
-                            label_visibility="collapsed",
-                        )
+    # Action Buttons for the Flow
+        act_c1, act_c2, act_c3 = st.columns([1,1,2])
+        nodes_dict = {n.id: n for n in st.session_state.flow_state.nodes}
+        st.write(nodes_dict)
+        st.write(nodes_dict.get("input_node"))
+        
+        if act_c1.button("AUTOCONNECT", use_container_width=True):
+            
+            new_edges = []
 
-                        if QuCAD_option == "Start Fresh":
-                            st.warning("You have selected QUCAD which require the day of the noise you want to simulate.")
-                            
-                        elif QuCAD_option == "User Previous QNN optimization":
-                            # st.write("huh")
-                            model_name = st.text_input("Enter your unique name for this QuCAD model you want to use :")
+            # Stage 1: Define Sequence Participants
+            # Only one from each category can exist due to the unique ID system
+            input_n = nodes_dict.get("input_node")
+            backend_n = nodes_dict.get("backend_node")
+            
+            # Find which compression node is present
+            comp_n = next((nodes_dict[i] for i in ["compress_node", "qucad_node", "nocomp_node"] if i in nodes_dict), None)
+            
+            # Find which fidelity node is present
+            fid_n = next((nodes_dict[i] for i in ["qbound_node", "simple_node"] if i in nodes_dict), None)
+            
+            transpile_n = nodes_dict.get("transpile_node")
 
-                    if 'qbound' in str(node_name):
-                        Qubound_option = st.radio(
-                            label= 'fid_option',
-                            options = ["Use Pretrained QuBound Model", "Start Fresh QuBound"],
-                            index = 1, 
-                            horizontal = True, 
-                            label_visibility = 'collapsed'
-                        )
-                        if Qubound_option == "Start Fresh QuBound":
-                            st.warning("You have selected QUCAD which require the day of the noise you want to simulate.")
-                        elif Qubound_option == 'Use Pretrained QuBound Model':
-                            model_name = st.text_input("Enter your unique name for this QuCAD model you want to use :")
-                    
-                    if 'transpile' in str(node_name):
-                        optim_map = {
-                            "0: NO QISKIT OPTMIZATION": 0,
-                            "1: LIGHT OPTMIZATION; Basic gate folding": 1,
-                            "2: MEDIUM; Commutative analysis": 2,
-                            "3: HEAVY; Deep pulse-levl Optimization": 3
-                        }
-                        optmization_choice = st.selectbox("Select qiskit Optimization Level:", options=list(optim_map.keys()), label_visibility="hidden")
-                    
-                    if 'simple' in str(node_name):
-                        pass
-                    if 'compress' in str(node_name):
-                        pass
-                    if 'nocomp' in str(node_name):
-                        pass
+            # Stage 2: Build the Main Chain
+            if input_n:
+                current_source = input_n.id
+                
+                # Input -> Compression
+                if comp_n:
+                    new_edges.append(StreamlitFlowEdge(f"edge-{current_source}-{comp_n.id}", current_source, comp_n.id, animated=True))
+                    current_source = comp_n.id
+                
+                # (Input or Compression) -> Fidelity
+                if fid_n:
+                    new_edges.append(StreamlitFlowEdge(f"edge-{current_source}-{fid_n.id}", current_source, fid_n.id, animated=True))
+                    current_source = fid_n.id
+                
+                # (Input or Compression or Fidelity) -> Transpile
+                if transpile_n:
+                    new_edges.append(StreamlitFlowEdge(f"edge-{current_source}-{transpile_n.id}", current_source, transpile_n.id, animated=True))
+
+            # Stage 3: Mandatory Backend Connections
+            if backend_n:
+                if "qucad_node" in nodes_dict:
+                    new_edges.append(StreamlitFlowEdge("backend-to-qucad", backend_n.id, "qucad_node", animated=True, label="Noise Data"))
+                if "qbound_node" in nodes_dict:
+                    new_edges.append(StreamlitFlowEdge("backend-to-qbound", backend_n.id, "qbound_node", animated=True, label="Noise Data"))
+
+            st.session_state.flow_state.edges = new_edges
+            st.rerun()
+
+        if act_c2.button("CLEAR CANVAS", use_container_width=True):
+            st.session_state.flow_state = StreamlitFlowState([], [])
+            st.rerun()
+
+if __name__ == "__main__":
+    flow_page()
